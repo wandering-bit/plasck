43acbae6a08e0885ffe9ba87d4b697f52433981e
Shivansh_Mishra <shivanshm442@gmail.com>
2021-07-28 10:42:49 +0530

    splash screen changes

---
+++ app/src/main/java/com/comp/track/plasck/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.comp.track.plasck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_splash_screen);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+
+                    sleep(4 * 1000);
+
+
+                    Intent i = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(i);
+
+                    finish();
+                } catch (Exception ignored) {
+                }
+            }
+        };
+
+        background.start();
+    }
+}
